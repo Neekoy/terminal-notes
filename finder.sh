3429cfa7352a9976230f135d8b664b119691e6ca
Neekoy <hristiyan.mihaylov@gmail.com>
2017-09-10 06:35:03 +0300

    Fixed so the script would work for any directory

--- finder.sh
+++ finder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-path=$(pwd)
-for each in $(find ${pwd}/content/ -type f -iname "*${1}*" -iname "*${2}*");
+SCRIPT=$(realpath $0)
+SCRIPTPATH=$(dirname $SCRIPT)
+for each in $(find ${SCRIPTPATH}/content/ -type f -iname "*${1}*" -iname "*${2}*");
 do echo;
 eachUpper=$(echo ${each} | tr 'a-z' 'A-Z')
 echo "$(tput setaf 3)$(tput bold)$(basename $eachUpper) $(tput sgr0)";
