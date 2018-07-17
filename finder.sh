9a00528a7b08bf300d98fa248fcf8cae9b5b9515
Hristiyan Mihaylov <hristiyan.mihaylov@gmail.com>
2018-07-17 13:47:34 +0300

    Refactored a bit and uploaded new notes

--- finder.sh
+++ finder.sh
@@ -8,11 +8,12 @@ fi
 
 SCRIPT=$(realpath $0)
 SCRIPTPATH=$(dirname $SCRIPT)
+
 for each in $(find ${SCRIPTPATH}/content/ -type f -iname "*${1}*" -iname "*${2}*");
-do echo;
-eachUpper=$(echo ${each} | tr 'a-z' 'A-Z')
-echo "$(tput setaf 3)$(tput bold)$(basename $eachUpper) $(tput sgr0)";
-echo;
-cat $each;
-echo
+    do echo;
+    eachUpper=$(echo ${each} | tr 'a-z' 'A-Z')
+    echo "$(tput setaf 3)$(tput bold)$(basename $eachUpper) $(tput sgr0)";
+    echo;
+    cat $each;
+    echo -e "\n$(tput setaf 3)$(tput bold)---------------------------------------- $(tput sgr0)\n";
 done
